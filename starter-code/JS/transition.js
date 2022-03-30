13a75c5c6bcf12890f3126d07b74eccaec207163
MazzGuille <mazzcode@gmail.com>
2022-03-30 01:10:55 -0300

    home page animations ready

--- starter-code/JS/transition.js
+++ starter-code/JS/transition.js
@@ -3,9 +3,9 @@ const bodyMoon = document.querySelector('.body-moon');
 const bodyCrew = document.querySelector('.body-crew');
 const bodyTechnology = document.querySelector('.body-technology'); */
 
-window.addEventListener('load', () => {
+/* window.addEventListener('load', () => {
     gsap.from('.body-home', { duration: 3, opacity: '0', ease: 'power1'});
-});
+}); */
 
 window.addEventListener('load', () => {
     gsap.from('.body-moon', { duration: 3, opacity: '0', ease: 'power1'});
