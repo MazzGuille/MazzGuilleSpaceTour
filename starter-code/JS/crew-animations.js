4f8aca6bd517dec623fafa2125bc88c002a1b71d
MazzGuille <mazzcode@gmail.com>
2022-04-10 04:24:03 -0300

    mobile 100% finished and fixed

--- starter-code/JS/crew-animations.js
+++ starter-code/JS/crew-animations.js
@@ -5,8 +5,7 @@ gsap.registerPlugin(ScrollTrigger);
 const commander = gsap.timeline({ defaults: { duration: 2 } });
 commander
 .from('.crew-title', {y: -65, opacity: 0})
-.from('.commander-img-container', {opacity: 0}, '-=1.5')
-.from('.commander-img',  {y: 65, opacity: 0}, '-=1.5')
+.from('.commander-img-container', {y: 65, opacity: 0}, '-=1.5')
 .from('.dot-container-commander', {opacity: 0}, '-=1.5')
 .from('.commander-title',{x: -80}, '-=1.5')
 .from('.commander-name',{x: 80}, '-=1.5')
@@ -25,8 +24,7 @@ const specialist = gsap.timeline(
 });
 
 specialist
-.from('.specialist-img-container', {duration: 2, opacity: 0})
-.from('.specialist-img',  {duration: 2, y: 65, opacity: 0}, '-=1.5')
+.from('.specialist-img-container', {y: 65,duration: 2, opacity: 0})
 .from('.dot-container-specialist', {duration: 2, opacity: 0}, '-=1.5')
 .from('.specialist-title',{duration: 2, x: -80}, '-=1.5')
 .from('.specialist-name',{duration: 2, x: 80}, '-=1.5')
@@ -45,8 +43,7 @@ const pilot = gsap.timeline(
 });
 
 pilot
-.from('.pilot-img-container', {duration: 2, opacity: 0})
-.from('.pilot-img',  {duration: 2, y: 65, opacity: 0}, '-=1.5')
+.from('.pilot-img-container', {y: 65, duration: 2, opacity: 0})
 .from('.dot-container-pilot', {duration: 2, opacity: 0}, '-=1.5')
 .from('.pilot-title',{duration: 2, x: -80}, '-=1.5')
 .from('.pilot-name',{duration: 2, x: 80}, '-=1.5')
@@ -65,8 +62,7 @@ const engineer = gsap.timeline(
 });
 
 engineer
-.from('.engineer-img-container', {duration: 2, opacity: 0})
-.from('.engineer-img',  {duration: 2, y: 65, opacity: 0}, '-=1.5')
+.from('.engineer-img-container', {y: 65, duration: 2, opacity: 0})
 .from('.dot-container-engineer', {duration: 2, opacity: 0}, '-=1.5')
 .from('.engineer-title',{duration: 2, x: -80}, '-=1.5')
 .from('.engineer-name',{duration: 2, x: 80}, '-=1.5')
--- starter-code/JS/tech-animations.js
+++ starter-code/JS/tech-animations.js
@@ -1,17 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
 
 /* --------------------VEHICLE GSAP------------------------ */
 const vehicle = gsap.timeline({ defaults: { duration: 2 } });
 vehicle
 .from('.tech-title', {y: 65, opacity: 0})
-.from('.vehicle-img-container', {x: -300, opacity: 0}, '-=1')
+.from('.vehicle-img-container', {scale: 0, opacity: 0}, '-=1')
 .from('.vehicle-one', {y: -65, opacity: 0}, '-=1.5')
 .from('.vehicle-two', {y: -65, opacity: 0}, '-=1.5')
 .from('.vehicle-three', {y: -65, opacity: 0}, '-=1.5')
 .from('.vehicle-sub1', {scale: 1.5, opacity: 0}, '-=1.5')
 .from('.vehicle-title',{scale: 1.5, opacity: 0}, '-=1.5')
-.from('.vehicle-txt', {scale: 1.5, opacity: 0}, '-=1.5');
+.to('.vehicle-txt', {duration: 10, text: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!", ease: "power2.out"}, '-=1');
 
 /* --------------------PORT GSAP------------------------ */
 const port = gsap.timeline(
@@ -25,13 +26,13 @@ const port = gsap.timeline(
 });
 
 port
-.from('.port-img-container', {duration: 2, x: -300, opacity: 0})
+.from('.port-img-container', {duration: 2, scale: 0, opacity: 0})
 .from('.port-one',  {duration: 2, y: -65, opacity: 0}, '-=1.5')
 .from('.port-two',  {duration: 2, y: -65, opacity: 0}, '-=1.5')
 .from('.port-three',  {duration: 2, y: -65, opacity: 0}, '-=1.5')
 .from('.port-sub1',{duration: 2, scale: 1.5, opacity: 0}, '-=1.5')
 .from('.port-title',{duration: 2, scale: 1.5, opacity: 0}, '-=1.5')
-.from('.port-txt', {duration: 2, scale: 1.5, opacity: 0}, '-=1.5');
+.to('.port-txt', {duration: 10, text: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.", ease: "power2.out"}, '-=1');
 
 /* --------------------CAPSULE GSAP------------------------ */
 const capsule = gsap.timeline(
@@ -45,12 +46,12 @@ const capsule = gsap.timeline(
 });
 
 capsule
-.from('.capsule-img-container', {duration: 2, x: -300, opacity: 0})
+.from('.capsule-img-container', {duration: 2, scale: 0.1, opacity: 0})
 .from('.capsule-one',  {duration: 2, y: -65, opacity: 0}, '-=1.5')
 .from('.capsule-two',  {duration: 2, y: -65, opacity: 0}, '-=1.5')
 .from('.capsule-three',  {duration: 2, y: -65, opacity: 0}, '-=1.5')
 .from('.capsule-sub1',{duration: 2, scale: 1.5, opacity: 0}, '-=1.5')
 .from('.capsule-title',{duration: 2, scale: 1.5, opacity: 0}, '-=1.5')
-.from('.capsule-txt', {duration: 2, scale: 1.5, opacity: 0}, '-=1.5');
+.to('.capsule-txt', {duration: 10, text: " A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.", ease: "power2.out"}, '-=1');
 
 
